86f5b3c3886fcadb76ba899cc72d8de88a162d0d
amanda <amandabcalado@icloud.com>
2022-12-21 15:52:52 -0300

    - Adicionado filtros na seleção da fonte de imagem

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def arte_jogo(jogos):
     for jogo in jogos:
         busca = search(f'{jogo} gamefaqs')
         for item in busca:
+            if '.com/boards' in item:
+                continue
             if 'gamefaqs.gamespot.com' in item:
                 linkgf = item.replace('/faqs', '')
                 linkgf = linkgf.replace('/videos', '')
@@ -20,9 +22,11 @@ def arte_jogo(jogos):
                 linkgf = linkgf.replace('/playing', '')
                 linkgf = linkgf.replace('/credit', '')
                 linkgf = linkgf.replace('/data', '')
+                linkgf = linkgf.replace('/news', '')
                 break
             else:
                 pass
+        headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54'}
         content = requests.get(f'{linkgf}/boxes', headers=headers).content
         site = BeautifulSoup(content, 'html.parser')
         ol = site.find('ol', class_='list')
@@ -30,3 +34,5 @@ def arte_jogo(jogos):
         imagem = 'https://gamefaqs.gamespot.com' + imagem['src']
         imagem = imagem.replace('thumb.jpg', 'front.jpg')
         print(imagem)
+
+# arte_jogo(['undertale','alice madness returns', 'slime rancher'])
